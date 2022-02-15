d8577305e0eeb82204e7923281d2c64096a5dfba
Rupertjeff1 <89801313+Rupertjeff1@users.noreply.github.com>
2022-02-15 12:49:25 -0500

    update

--- game.py
+++ game.py
@@ -1,14 +1,15 @@
 print("Hello, welcome to my game. ")
 
 playing = input("Do you want to play? ")
-    if playing != "yes":
-        quit()
-    print("Okay, great! Lets play")
+if playing.lower() != "yes":
+    quit()
+print("Okay, lets play!")
 
 answer = input("What is the Capital City of Ohio?")
-if answer == "Columbus":
+if answer.lower() == "Columbus":
     print("Correct!")
+    score += 1
 else: 
     print("Sorry, that's not correct.")
 
-    
+print("You got " + str(score) + " questions correct!")
