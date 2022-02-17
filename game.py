267d255e606c6e50c9d64e6847bdfad2e2618540
Rupertjeff1 <89801313+Rupertjeff1@users.noreply.github.com>
2022-02-16 22:14:25 -0500

    Add files via upload

--- game.py
+++ game.py
@@ -1,13 +1,32 @@
+import time
 print("Hello, welcome to my game. ")
 
-playing = input("Do you want to play? ")
+playing = input("Do you want to play States and Capitals? ")
 if playing.lower() != "yes":
     quit()
 print("Okay, lets play!")
+
+time.sleep(1)
+print("The objective of this game is I will give you either State or Capital. If I give you a State you must guess its Capital City. \
+If I give you a Capital, you must then its State.")
 score = 0
 
-answer = input("What is the Capital City of Ohio?")
-if answer.lower() == "Columbus":
+answer = input("What is the Capital City of Ohio? ")
+if answer.lower() == "columbus":
+    print("Correct!")
+    score += 1
+else: 
+    print("Sorry, that's not correct.")
+
+answer = input("What States Capital is Harrisburgh? ")
+if answer.lower() == "pennsylvania":
+    print("Correct!")
+    score += 1
+else: 
+    print("Sorry, that's not correct.")
+
+answer = input("What is the Capital of Florida? ")
+if answer.lower() == "tallahasee":
     print("Correct!")
     score += 1
 else: 
